291cf676c5d2f5a46ae60c43f1656142ddb871b7
charlesgobina <charles.gobina@gmail.com>
2025-01-12 02:15:03 +0200

    fix: update the developer model to include id and isDevReady attributes

--- model/developerModel.js
+++ model/developerModel.js
@@ -2,36 +2,49 @@ import joi from 'joi';
 
 class DeveloperModel {
     constructor(
+      id,
       email,
       password,
-      companyName
+      companyName,
+      isDevReady=false
+
     ) {
+        this.id = id;
         this.email = email;
         this.password = password;
         this.companyName = companyName;
+        this.isDevReady = isDevReady;
+
     }
 
     toJSON() {
         return {
+            id: this.id,
             email: this.email,
             password: this.password,
-            companyName: this.companyName
+            companyName: this.companyName,
+            isDevReady: this.isDevReady
         };
     }
 
     static get developerSchema() {
         return joi.object({
+            id: joi.string(),
             email: joi.string().required(),
             password: joi.string().required(),
-            companyName: joi.string()
+            companyName: joi.string(),
+            isDevReady: joi.boolean()
+
         });
     }
 
     static get updateDeveloperSchema() {
         return joi.object({
+            id: joi.string(),
             email: joi.string(),
             password: joi.string(),
-            companyName: joi.string()
+            companyName: joi.string(),
+            isDevReady: joi.boolean()
         });
     }
 
