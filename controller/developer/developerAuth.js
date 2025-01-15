e2090dad6b46b33ae7c2be6d77fe202a6d2c77e7
charlesgobina <charles.gobina@gmail.com>
2025-01-15 14:19:20 +0200

    chore: rebuild registration class based on client sdk

--- controller/developer/developerAuth.js
+++ controller/developer/developerAuth.js
@@ -1,73 +1,65 @@
 import DeveloperModel from "../../model/developerModel.js";
 import DeveloperService from "../../services/developerService.js";
-import { verifyPassword } from "../../utils/passwordHashing.js";
-import { fbAdmin } from "../../config/config.js";
 import { hashPassword } from "../../utils/passwordHashing.js";
-import { createJWT } from "../../utils/jwt.js";
-import { auth } from "../../config/config.js";
-import * as uuid from 'uuid'
-import { trimmer, validateEmail, validatePassword } from "../../utils/emailAndPasswordValidationHelper.js";
-
-
+import { AuthManager } from "../../services/authmanager.js";
+import {
+  validateEmail,
+  validatePassword,
+} from "../../utils/emailAndPasswordValidationHelper.js";
+import app from "../../config/config.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import ora from 'ora';
+import chalk from "chalk";
 
 class DevAuth {
+  constructor() {
+    this.auth = getAuth(app);
+  }
 
   // for express
-  static async signup(req, res) {
-    const developerId = uuid.v4()
-    const { email, password, companyName } = req.body
-    validateEmail(email)
-    validatePassword(password)
+  async signup(email, password, companyName) {
+    let currentUser = null;
+
+    validateEmail(email);
+    validatePassword(password);
+
+    const spinner = ora('Signing up...').start();
+    
     try {
-      // create developer account
-      const user = await fbAdmin.auth().createUser({
-        uid: developerId,
-        email: email,
-        password: password,
-        emailVerified: false
-      })
-
-      // save developer data to firestore
-      if (user.uid != null) {
-
-        // hash password
-        const hashedPassword = hashPassword(password)
-        const developerData = new DeveloperModel(developerId, email, hashedPassword, companyName)
-        DeveloperService.saveDeveloperData(developerId, developerData.toJSON())
+      const userCredential = await createUserWithEmailAndPassword(
+        this.auth,
+        email,
+        password
+      );
+      currentUser = userCredential.user;
+      // hash password
+      const hashedPassword = hashPassword(password);
+      const developerData = new DeveloperModel(
+        currentUser.uid,
+        email,
+        hashedPassword,
+        companyName
+      );
+      DeveloperService.saveDeveloperData(
+        currentUser.uid,
+        developerData.toJSON()
+      );
+      spinner.succeed(chalk.green("Account created successfully!"));
+      try {
+        const spinner = ora('Logging in...').start();
+        const authManager = new AuthManager();
+        await authManager.login(email, password);
+        spinner.succeed(chalk.green("Login successful!"));
+      } catch (error) {
+        spinner.fail(chalk.red(`Login failed: ${error.message}`));
       }
-
-      // generate jwt token
-      const token = createJWT(user)
-
-      console.log(user)
-      res.status(201).json({ token: token })
-    }
-    catch (error) {
-      console.log(error)
-      res.send(error.message)
-    }
-  }
-
-  
-
-  static async login(req, res) {
-    const { email, password } = req.body
-    try {
-      const user = await DeveloperService.getDeveloperByEmail(email)
-      const newDeveloper = new DeveloperModel(user.id, user.email, user.password.hashedPassword, user.companyName)
-      // compare password
-      if (!verifyPassword(password, newDeveloper.password, user.password.salt)) {
-        res.status(401).json({ message: 'Invalid email or password' })
-        return
-      }
-      await DeveloperService.updateDevReadyStatus(newDeveloper.id, true)
-      res.status(200).json({ message: 'Login successful' })
     } catch (error) {
-      console.log(error)
-      res.send(error.message)
+      spinner.fail(chalk.red(`Registration failed: ${error.message}`));
     }
   }
-
 }
 
-export { DevAuth }
+export { DevAuth };
