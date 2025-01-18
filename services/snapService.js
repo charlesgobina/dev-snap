8fbae31b3fb430f400999132f57803047045c7f9
charlesgobina <charles.gobina@gmail.com>
2025-01-18 23:36:41 +0200

    chore: add function to fetch snaps from firestore

--- services/snapService.js
+++ services/snapService.js
@@ -13,6 +13,25 @@ class SnapService {
       console.log(e)
     }
   }
+
+  static async getSnap(userId, snapId) {
+    try {
+      const snap = await db.collection(this.snapCollection).doc(userId).collection(this.snapSubcollection).doc(snapId).get()
+      return snap.data()
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  // get all snaps
+  static async getAllSnaps(userId) {
+    try {
+      const snaps = await db.collection(this.snapCollection).doc(userId).collection(this.snapSubcollection).get()
+      return snaps.docs.map(doc => doc.data())
+    } catch (e) {
+      console.log(e)
+    }
+  }
 }
 
 export default SnapService
